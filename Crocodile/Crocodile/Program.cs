26c45f80a8793bb9595f8b3a4422bf0371eb3c02
ASUS <ASUS@DESKTOP-2AL4IC0>
2024-04-05 12:05:39 +0330

    Tohid noori | getting input from user

--- Crocodile/Crocodile/Program.cs
+++ Crocodile/Crocodile/Program.cs
@@ -10,6 +10,21 @@ namespace Crocodile
     {
         static void Main(string[] args)
         {
+            //Getting quera input
+            int numberOfTestCase = int.Parse(Console.ReadLine());
+            List<List<int>> dataset = new List<List<int>>();
+
+            //Putting each line of data in the dataset
+            for(int i = 0; i < numberOfTestCase; i++)
+            {
+                String line = Console.ReadLine();
+                String[] dataLine = line.Split(' ');
+                dataset.Add(new List<int>());
+                for (int j = 0; j < dataLine.Length; j++)
+                {
+                    dataset[i].Add(int.Parse(dataLine[j]));   
+                }
+            }
         }
     }
 }
