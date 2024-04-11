7162e3aa1a4bc6f82d2941df94e65053ab8fb769
mohammadaminnasiri131383@gmail.com <mohammadaminnasiri131383@gmail.com>
2024-04-11 17:12:13 +0330

    Amin Nasiri | Changed algorithm to divide and conquer

--- Naghoos/Program.cs
+++ Naghoos/Program.cs
@@ -9,7 +9,7 @@ namespace MyApp
             int n = int.Parse(Console.ReadLine());
             int[,] matrix = ReadMatrix(n);
             int[,] matrixPowerThree = MultiplyMatrices(matrix, MultiplyMatrices(matrix, matrix, n), n);
-            
+
             if (CycleLengthThree(matrixPowerThree, n))
             {
                 Console.WriteLine("YES");
@@ -23,7 +23,7 @@ namespace MyApp
         {
             for (int i = 0; i < n; i++)
             {
-                if (matrixPowerThree[i,i] != 0)
+                if (matrixPowerThree[i, i] != 0)
                 {
                     return true;
                 }
@@ -32,8 +32,57 @@ namespace MyApp
             return false;
         }
 
-        // Multiplying Matrix1 into Matrix2
+        // Multiplying Matrix1 into Matrix2 using divide and conquer
         public static int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2, int n)
+        {
+            int[,] result = new int[n, n];
+
+            if (n == 1)
+            {
+                result[0, 0] = matrix1[0, 0] * matrix2[0, 0];
+            }
+            else
+            {
+                int[,] A11 = new int[n / 2, n / 2];
+                int[,] A12 = new int[n / 2, n / 2];
+                int[,] A21 = new int[n / 2, n / 2];
+                int[,] A22 = new int[n / 2, n / 2];
+
+                int[,] B11 = new int[n / 2, n / 2];
+                int[,] B12 = new int[n / 2, n / 2];
+                int[,] B21 = new int[n / 2, n / 2];
+                int[,] B22 = new int[n / 2, n / 2];
+
+                // Divide matrix1 into 4 submatrices
+                DivideMatrix(matrix1, A11, 0, 0);
+                DivideMatrix(matrix1, A12, 0, n / 2);
+                DivideMatrix(matrix1, A21, n / 2, 0);
+                DivideMatrix(matrix1, A22, n / 2, n / 2);
+
+                // Divide matrix2 into 4 submatrices
+                DivideMatrix(matrix2, B11, 0, 0);
+                DivideMatrix(matrix2, B12, 0, n / 2);
+                DivideMatrix(matrix2, B21, n / 2, 0);
+                DivideMatrix(matrix2, B22, n / 2, n / 2);
+
+                // Compute sub-results recursively
+                int[,] C11 = AddMatrices(MultiplyMatrices(A11, B11, n / 2), MultiplyMatrices(A12, B21, n / 2), n / 2);
+                int[,] C12 = AddMatrices(MultiplyMatrices(A11, B12, n / 2), MultiplyMatrices(A12, B22, n / 2), n / 2);
+                int[,] C21 = AddMatrices(MultiplyMatrices(A21, B11, n / 2), MultiplyMatrices(A22, B21, n / 2), n / 2);
+                int[,] C22 = AddMatrices(MultiplyMatrices(A21, B12, n / 2), MultiplyMatrices(A22, B22, n / 2), n / 2);
+
+                // Combine sub-results into the result matrix
+                CombineMatrices(C11, result, 0, 0);
+                CombineMatrices(C12, result, 0, n / 2);
+                CombineMatrices(C21, result, n / 2, 0);
+                CombineMatrices(C22, result, n / 2, n / 2);
+            }
+
+            return result;
+        }
+
+        // Function to add two matrices
+        public static int[,] AddMatrices(int[,] matrix1, int[,] matrix2, int n)
         {
             int[,] result = new int[n, n];
 
@@ -41,15 +90,58 @@ namespace MyApp
             {
                 for (int j = 0; j < n; j++)
                 {
-                    for (int k = 0; k < n; k++)
-                    {
-                        result[i, j] += matrix1[i, k] * matrix2[k, j];
-                    }
+                    result[i, j] = matrix1[i, j] + matrix2[i, j];
                 }
             }
 
             return result;
         }
+
+        // Function to divide a matrix into a submatrix
+        public static void DivideMatrix(int[,] matrix, int[,] submatrix, int startRow, int startCol)
+        {
+            int n = submatrix.GetLength(0);
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    submatrix[i, j] = matrix[startRow + i, startCol + j];
+                }
+            }
+        }
+
+        // Function to combine a submatrix into a matrix
+        public static void CombineMatrices(int[,] submatrix, int[,] matrix, int startRow, int startCol)
+        {
+            int n = submatrix.GetLength(0);
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[startRow + i, startCol + j] = submatrix[i, j];
+                }
+            }
+        }
+
+        //public static int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2, int n)
+        //{
+        //    int[,] result = new int[n, n];
+
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        for (int j = 0; j < n; j++)
+        //        {
+        //            for (int k = 0; k < n; k++)
+        //            {
+        //                result[i, j] += matrix1[i, k] * matrix2[k, j];
+        //            }
+        //        }
+        //    }
+
+        //    return result;
+        //}
 
         // Reading Matrix Line by Line Separated by ' '
         public static int[,] ReadMatrix(int n)
@@ -60,20 +152,21 @@ namespace MyApp
             {
                 string[] input = Console.ReadLine().Split(' ');
 
-                if (input.Length != n)
-                {
-                    Console.WriteLine($"Error: Expected {n} elements for row {i + 1}");
-                    return null;
-                }
+                //if (input.Length != n)
+                //{
+                //    Console.WriteLine($"Error: Expected {n} elements for row {i + 1}");
+                //    return null;
+                //}
 
                 for (int j = 0; j < n; j++)
-                {
-                    if (!int.TryParse(input[j], out matrix[i, j]))
-                    {
-                        Console.WriteLine($"Error: Invalid input '{input[j]}'");
-                        return null;
-                    }
-                }
+                    matrix[i, j] = int.Parse(input[j]);
+                //{
+                //    if (!int.TryParse(input[j], out matrix[i, j]))
+                //    {
+                //        Console.WriteLine($"Error: Invalid input '{input[j]}'");
+                //        return null;
+                //    }
+                //}
             }
 
             return matrix;
